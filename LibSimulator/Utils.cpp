c9ab6f62d3fe19d93f3c447346e5365d918e4af6
藤原良樹 <yoshikif98@gmail.com>
2022-05-23 23:34:10 +0900

    input transformation

--- LibSimulator/Utils.cpp
+++ LibSimulator/Utils.cpp
@@ -16,10 +16,12 @@ namespace simulator
     for (int input_channel = 0; input_channel < num_input_channel; input_channel++){
       for (int input_height = 0; input_height < num_input_height; input_height++){
         for (int input_width = 0; input_width < num_input_width; input_width++){
-          int memoryIndex = ((input_width < (num_input_width/2 + num_input_width%2)) ? 1 : 0) +
-              2 * ((input_height < (num_input_height/2 + num_input_width%2)) ? 1 : 0 );
-          int width = (memoryIndex % 2 == 0) ? input_width : input_width + num_input_width / 2 + 1;
-          int height = 0;
+          bool heightFirstGroup = input_height < (num_input_height / 2) + (num_input_height % 2);
+          bool widthFirstGroup = input_width < (num_input_width / 2) + (num_input_width % 2);
+
+          int memoryIndex = 2 * ((heightFirstGroup) ? 0 : 1) + ((widthFirstGroup) ? 0 : 1);
+          int height = (heightFirstGroup) ? input_height : input_height - ((num_input_height / 2) + (num_input_height % 2));
+          int width = (widthFirstGroup) ? input_width : input_width - ((num_input_height / 2) + (num_input_height % 2));
           int channelGroup = input_channel / num_PE_parallel;
           inputMemories[memoryIndex][height][width][channelGroup][input_channel % channelGroup] = inputValues[input_channel][input_height][input_width];
         }
